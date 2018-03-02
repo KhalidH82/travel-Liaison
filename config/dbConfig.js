a72bf3b1ac9d4128229fbcd2cdaa0d0285d7e440
Khalid Hosein <khalid.hosein9@gmail.com>
2018-03-02 10:38:43 -0500

    deploy heroku

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,17 @@
-module.exports = process.env.DATABASE_URL || {
-  host:     process.env.DB_HOST || 'localhost',
-  port:     process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'travelDB',
-  /* user:     process.env.DB_USER, */
-};
+function setDatabase() {
+  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    // this would be the config of the DB when in development on our machines
+    return {
+      database: 'travelDB',
+      port: 5432,
+      host: 'localhost',
+    };
+  } else if (process.env.NODE_ENV === 'production') {
+    // HEROKU sets process.env.NODE_ENV to 'production' once pushed to that env
+    return process.env.DATABASE_URL;
+  }
+}
+
+const db = setDatabase();
+
+module.exports = db;
