c8bb61464c1ef575c9c9ae8f96a6a92789811911
Khalid Hosein <khalid.hosein9@gmail.com>
2018-03-04 11:12:31 -0500

    Brought in google maps API

--- public/src/main.js
+++ public/src/main.js
@@ -1,34 +1,13 @@
-$(()=> {
+    function initMap() {
+        // Create a map object and specify the DOM element for display.
+        let map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
 
-  $('button').on("click", 
-  function produceSearch(term) {
-    var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316)
-     let map_inst = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
-    });
-
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    query: 'restaurant',
-    key: 'AIzaSyDD-DAAMzpneF3Oz_uoCuuiXNYGyng6Vh8'
-  };
-
-  let service = new google.maps.places.PlacesService(map_inst);
-  service.textSearch(request, callback);
-  })
-
-  function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
-}
-
- 
-
-})
+function activatePlacesSearch(){
+  let input = document.getElementById('search_term');
+  let autocomplete = new google.maps.places.Autocomplete(input);
+};
 
